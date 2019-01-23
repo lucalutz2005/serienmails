f09d0b7a856e42adbace7633814e82e135cf23aa
lucalutz2005 <46973971+lucalutz2005@users.noreply.github.com>
2019-01-23 20:25:25 +0100

    Delete lade_ein.sh

--- transaktionen/lade_ein.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-hallo='USE serienmails; INSERT INTO `events` (`name`, `inhalt_pfad`, `anhang_pfad`) VALUES ('$1', '$2', NULL);';
-echo 'USE serienmails; INSERT INTO `events` (`name`, `inhalt_pfad`, `anhang_pfad`) VALUES ('$1', '$2', NULL);'             | mysql -h localhost -u serienmails --password="garnix"
-echo 'Veranstaltungen: (Tabelle: Events)'
-echo "USE serienmails; SELECT * FROM  events;"                                                            | mysql -h localhost -u serienmails --password="garnix"
